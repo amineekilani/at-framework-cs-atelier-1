771716448f0de2e7a8e4b28c3e23188eb9dbe743
Amine Kilani <aminekilani901@gmail.com>
2025-02-17 11:25:03 +0100

    17/02

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BlogController extends AbstractController
+{
+    #[Route("/blog/article", name: "blog")]
+    public function index(): Response
+    {
+        return $this->render("blog/index.html.twig");
+    }
+    #[Route("/blog/article/{id<\d+>?1}", name: "blog_article_show")]
+    public function show($id): Response
+    {
+        return $this->render("blog/show.html.twig", ["id"=>$id]);
+    }
+    #[Route("/blog/article/lister", name: "blog_article_lister")]
+    public function lister(): Response
+    {
+        return $this->render("blog/lister.html.twig");
+    }
+    #[Route("/blog/article/detail/{id<[0-9]{1,3}>}/{name<Belaili|Sasse>}", name: "blog_article_detail")]
+    public function detail($id, $name): Response
+    {
+        return $this->render("blog/detail.html.twig", ["id"=>$id, "name"=>$name]);
+    }
+    #[Route("/blog/test", name: "blog_test")]
+    public function test(): Response
+    {
+        //$url=$this->generateUrl("blog");
+        //return $this->redirect($url);
+        //return $this->redirect($this->generateUrl("blog"));
+        return $this->redirectToRoute("blog_article_detail", ["id"=>10, "name"=>"Sasse"]);
+    }
+}
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,23 +1,25 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class TestController
+class TestController extends AbstractController
 {
-    #[Route("afficher/fr", name: "test_fr")]
-    function afficher()
+    #[Route("afficher/fr/{name}", name: "test_fr")]
+    function afficher($name)
     {
-        dd("Bonjour tout le monde");
+        dd("<h1>Bonjour $name</h1>");
     }
     #[Route("afficher/en", name: "test_en")]
     function show()
     {
-        dd("Hello World");
+        return new Response("<h1>Hello</h1>");
     }
-    #[Route("afficher/ar", name: "test_ar")]
-    function tfarrej()
+    #[Route("afficher/ar/{nom}", name: "test_ar")]
+    function tfarrej($nom)
     {
-        dd("3asslema");
+        return $this->render("Test/tfarrej.html.twig", ["nom"=>$nom]);
     }
 }
